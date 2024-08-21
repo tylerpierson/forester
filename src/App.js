8a9030d2d64836a275d20c54e5c8747df1e7f30d
Bryce Sexton <brycesexton15@gmail.com>
2024-08-21 14:59:02 -0700

    font added, minor cosmetic changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
         <div className={styles.App}>
             <img className={styles.logo} src='https://images.squarespace-cdn.com/content/v1/5c708346d74562229cb7c872/1555020988289-4094VIM7BKQ2AWU6QOBG/Forester+logo2.png?format=1500w' alt='Forester Logo' />
             <NavBar />
-            {/* add video here with social icons overlayed */}
+            {/* add a video here with social icons overlayed */}
             <SocialMedia />
         </div>
     );
